06b7176ff509427e2e47706354cac0442c92d5f8
PreethamAR <yes.you.can.preetham@gmail.com>
2026-03-11 23:40:50 +0530

    Euclidean and Consecutive algorithms - Using loops and Recursion

---
+++ GCD.c
@@ -0,0 +1,85 @@
+// Greatest Common Divisor(GCD) is the largest positive integer that divides two or more numbers without leaving a remainder.
+
+// 1.Algorithm – Using Loop Euclidean Method:-
+#include <stdio.h>
+
+int main() {
+int a, b, temp;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+
+while(b != 0) {
+    temp = b;
+    b = a % b;
+    a = temp;
+}
+printf("GCD = %d", a);
+return 0;
+}
+
+// Using Recursion
+
+#include <stdio.h>
+int gcd(int a, int b) {
+if(b == 0)
+return a;
+else
+return gcd(b, a % b);
+}
+int main() {
+int a, b;
+
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+
+printf("GCD = %d", gcd(a,b));
+
+return 0;
+}
+
+// Using Consecutive Integer Checking (Loop):-
+
+#include <stdio.h>
+
+int main() {
+int a, b, t;
+
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+
+t = (a < b) ? a : b;
+
+while(t > 0) {
+    if(a % t == 0 && b % t == 0) {
+        printf("GCD = %d", t);
+        break;
+    }
+    t--;
+}
+return 0;
+}
+
+// Using Recursion
+
+#include <stdio.h>
+
+int gcd(int a, int b, int t) {
+
+    if(t == 0)
+        return 1;
+
+    if(a % t == 0 && b % t == 0)
+        return t;
+
+    return gcd(a, b, t - 1);
+}
+int main() {
+
+int a, b, t;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+t = (a < b) ? a : b;
+printf("GCD = %d", gcd(a, b, t));
+
+return 0;
+}
