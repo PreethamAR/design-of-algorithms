e3fd09a481f302b033939b3dcb663271100e7058
PreethamAR <yes.you.can.preetham@gmail.com>
2026-03-12 23:04:43 +0530

    Middle school method

--- GCD.c
+++ GCD.c
@@ -2,7 +2,6 @@
 
 // 1.Algorithm – Using Loop Euclidean Method:-
 #include <stdio.h>
-
 int main() {
 int a, b, temp;
 printf("Enter two numbers: ");
@@ -82,4 +81,54 @@ t = (a < b) ? a : b;
 printf("GCD = %d", gcd(a, b, t));
 
 return 0;
+}
+
+// Middle School Method (Loop Implementation):-
+
+#include <stdio.h>
+int main() {
+int a, b, i = 2, gcd = 1;
+printf("Enter two numbers: ");
+scanf("%d %d", &a, &b);
+while(a > 1 && b > 1) {
+    if(a % i == 0 && b % i == 0) {
+        gcd = gcd * i;
+        a = a / i;
+        b = b / i;
+    }
+    else if(a % i == 0) {
+        a = a / i;
+    }
+    else if(b % i == 0) {
+        b = b / i;
+    }
+    else {
+        i++;
+    }
+}
+printf("GCD = %d", gcd);
+return 0;
+}
+
+// Middle School Method (Recursion Implementation):-
+
+#include <stdio.h>
+int gcd_middle(int a, int b, int i) {
+    if(a == 1 || b == 1)
+        return 1;
+    if(a % i == 0 && b % i == 0)
+        return i * gcd_middle(a/i, b/i, i);
+    if(a % i == 0)
+        return gcd_middle(a/i, b, i);
+    if(b % i == 0)
+        return gcd_middle(a, b/i, i);
+    return gcd_middle(a, b, i+1);
+}
+
+int main() {
+    int a, b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("GCD = %d", gcd_middle(a, b, 2));
+    return 0;
 }
